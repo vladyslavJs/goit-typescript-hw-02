027011f741c7d1c57dbb29654b568ec78281c291
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-09 12:06:04 +0200

    Update 3.ts

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,12 @@
-let some:unknown;
+let some: unknown;
 some = 'Text';
 let str: string;
-str = some;
+
+
+if (typeof some === 'string') {
+    str = some;
+} else {
+    console.log('Not a sting!')
+}
 
 export {};
