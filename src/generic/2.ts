3eff2d44f4b6ddd872e2cb2866e4c13785df5e7c
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-04-09 12:43:50 +0200

    Update 2.ts

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type TypeTop = Pick<AllType, 'name' | 'color'>
+type TypeBottom = Pick<AllType, 'position' | 'weight'>
+
+function compare (top: TypeTop, bottom: TypeBottom): AllType {
   return {
     name: top.name,
     color: top.color,
